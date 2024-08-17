0ede5c10e94bf1783494107824114fb4f9e7a1f2
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-08-17 17:57:42 +0500

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,3 @@
-
-
 import 'dart:convert';
 import 'dart:developer';
 import 'package:flutter/material.dart';
@@ -14,41 +12,35 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
-  List<RecipeModel> recipeList = <RecipeModel> [];
+  List<RecipeModel> recipeList = <RecipeModel>[];
 
   TextEditingController searchController = TextEditingController();
 
-
-  void getRecipe(String query) async{
-
+  void getRecipe(String query) async {
     recipeList.clear();
     //String url = 'https://api.edamam.com/api/recipes/v2?type=any&q=$query&app_id=6b0ebedc&app_key=c0375006048ed6bc6dae30a6282ca977&ingr=0-3&health=alcohol-free&calories=100-300';
-    String url = 'https://api.edamam.com/api/recipes/v2?type=any&q=$query&app_id=6b0ebedc&app_key=c0375006048ed6bc6dae30a6282ca977';
+    String url =
+        'https://api.edamam.com/api/recipes/v2?type=any&q=$query&app_id=6b0ebedc&app_key=c0375006048ed6bc6dae30a6282ca977';
     Response response = await get(Uri.parse(url));
     Map data = jsonDecode(response.body);
-   // log(data.toString());
-    data['hits'].forEach((element){
+    // log(data.toString());
+    data['hits'].forEach((element) {
       RecipeModel recipeModel = new RecipeModel();
       recipeModel = RecipeModel.fromMap(element['recipe']);
       recipeList.add(recipeModel);
       log(recipeList.toString());
     });
-    setState(() {
+    setState(() {});
 
-    });
-
-    recipeList.forEach((Recipe){
+    recipeList.forEach((Recipe) {
       print(Recipe.applabel);
-
     });
-
-          }
+  }
 
   @override
   void initState() {
     super.initState();
-      getRecipe('Apple');
+    getRecipe('Apple');
   }
 
   @override
@@ -60,72 +52,142 @@ class _HomeState extends State<Home> {
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height,
             decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  colors:  [
-                      Color(0xffec458d), Color(0xdd474ed7),
-                  ]
-              ),
+              gradient: LinearGradient(colors: [
+                Color(0xffec458d),
+                Color(0xdd474ed7),
+              ]),
             ),
           ),
-          Column(
-            children: [
-              //Search Bar ===========================================================
-              SafeArea(
-                child: Container(
-
-                  padding: EdgeInsets.symmetric(horizontal: 8),
-                  margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
-
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(24),
-                    color: Colors.white,
-                  ),
-
-                  child: Row(
-                    children: [
-                      GestureDetector(
-                        onTap: (){
-                          if((searchController.text).replaceAll(' ', '') == ''){
-                            print('Blanked');
-                          } else{
-                            getRecipe(searchController.text);
-                          }
-                        },
-                        child: Container(
-                          child: Icon(Icons.search, color: Colors.blue,),
-                          margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
-                        ),
-                      ),
-                      Expanded(
-                        child: TextField(
-                          controller: searchController,
-                          decoration: InputDecoration(
-                            hintText: 'Search Recipe',
-                            border: InputBorder.none
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                //Search Bar ===========================================================
+                SafeArea(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 8),
+                    margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(24),
+                      color: Colors.white,
+                    ),
+                    child: Row(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            if ((searchController.text).replaceAll(' ', '') ==
+                                '') {
+                              print('Blanked');
+                            } else {
+                              getRecipe(searchController.text);
+                            }
+                          },
+                          child: Container(
+                            child: Icon(
+                              Icons.search,
+                              color: Colors.blue,
+                            ),
+                            margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
                           ),
                         ),
+                        Expanded(
+                          child: TextField(
+                            controller: searchController,
+                            decoration: InputDecoration(
+                                hintText: 'Search Recipe',
+                                border: InputBorder.none),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('WHAT DO YOU WANT TO COOK TODAY?',
+                          style: TextStyle(fontSize: 33, color: Colors.white)),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Text(
+                        'Lets cook something!',
+                        style: TextStyle(fontSize: 20, color: Colors.white),
                       )
                     ],
                   ),
                 ),
-              ),
+                Container(
+                  child: ListView.builder(
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: recipeList.length,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                          child: Card(
+                            margin: EdgeInsets.all(20),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            elevation: 0.0,
+                            child: Stack(
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: Image.network(
+                                    recipeList[index].appimgurl,
+                                    fit: BoxFit.cover,
+                                    width: double.infinity,
+                                    height: 200,
+                                  ),
+                                ),
+                                Positioned(
+                                    left: 0,
+                                    bottom: 0,
+                                    right: 0,
+                                    child: Container(
+                                        decoration: BoxDecoration(
+                                            color: Colors.black26),
+                                        padding: EdgeInsets.symmetric(
+                                            vertical: 5, horizontal: 10),
+                                        child: Text(
+                                          recipeList[0].applabel,
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 20),
+                                        ))),
 
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('WHAT DO YOU WANT TO COOK TODAY?',style: TextStyle(fontSize: 33,color: Colors.white)),
-                    SizedBox(height: 12,),
-                    Text('Lets cook something!',style: TextStyle(fontSize: 20,color: Colors.white),)
-                  ],
+                                Positioned(
+                                  right: 0,
+                                    child: Container(
+                                      width: 80,
+                                      height: 40,
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.only(
+                                          topRight: Radius.circular(10),
+                                          bottomLeft: Radius.circular(10),
+                                        )
+                                      ),
+                                        child: Center(
+                                          child: Row(
+                                            mainAxisAlignment: MainAxisAlignment.center,
+                                            children: [
+                                              Icon(Icons.local_fire_department,size: 16,),
+                                              Text(recipeList[index].appcalo.toString().substring(0,6)),
+                                            ],
+                                          ),
+                                        )))
+                              ],
+                            ),
+                          ),
+                        );
+                      }),
                 ),
-              )
-
-
-
-
-            ],
+              ],
+            ),
           )
         ],
       ),
@@ -133,5 +195,6 @@ class _HomeState extends State<Home> {
   }
 }
 
-
-
+Widget khan() {
+  return Text('Khan is good boy');
+}
